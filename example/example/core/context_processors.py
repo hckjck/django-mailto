5743ae8191b0f7a705c7f60827dcfdfaa2351acc
Michael Handerek <michael@handerek.net>
2015-04-25 11:51:22 +0200

    Remove example project from source.

--- example/example/core/context_processors.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.contrib.sites.models import Site
-
-
-def site(request):
-    return {
-        'site': Site.objects.get_current(),
-        'site_url': request.build_absolute_uri('/')
-    }
